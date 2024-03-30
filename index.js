26b363c02e587e6d6bf7bf92f5be83e152a0cfe0
lucas <lucasmacuna@gmail.com>
2024-03-30 00:24:28 -0300

    modificacion

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const app=require('./src/App/app.js');
 
 require("dotenv").config();
-app.set('port',process.env.PORT || 4000)
+const port=process.env.PORT || 4000
 
 
 
 app.use(require('./src/Router/Routes.js'))
-app.listen(app.get('port'),(error)=>{
+app.listen(port,(error)=>{
 
     if(error){
         console.log(error)
     }
 
-    console.log(`Servidor funcionando en el puerto ${app.get('port')}`)
+    console.log(`Servidor funcionando en el puerto ${port}`)
 })
 
